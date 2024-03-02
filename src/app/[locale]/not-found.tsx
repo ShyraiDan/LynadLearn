b022bd7a503fcb4aa8e0a2889f36761d6c77127d
ShirayDan <dan.shirayy@gmail.com>
2024-03-02 16:37:54 +0200

    created not found page

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,7 +1,28 @@
+import Image from 'next/image'
+import styles from './notFound.module.scss'
+import NavigationLink from '@/components/ui/NavigationLink/NavigationLink'
+
+import notFound from '@/assets/404.png'
+import { FaHome } from 'react-icons/fa'
+
 export default function NotFoundPage() {
   return (
-    <div>
-      <h1>Page not found aboba</h1>
+    <div className={styles.container}>
+      <div>
+        <Image src={notFound} alt='Page not found' />
+      </div>
+      <div className={styles.message}>
+        <h1>Page not found</h1>
+        <h4>Sorry, the page you were looking for does not exist.</h4>
+
+        <NavigationLink href='/' className={styles.link}>
+          <FaHome /> Back Home
+        </NavigationLink>
+
+        <span>
+          If the page is broken, <NavigationLink href='/contact-us'>Let us know</NavigationLink>.
+        </span>
+      </div>
     </div>
   )
 }
