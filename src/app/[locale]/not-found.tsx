59431202d7021e63521f76d3245b944e5501bb51
ShirayDan <dan.shirayy@gmail.com>
2024-03-03 11:20:18 +0200

    added localization for 404 page

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,26 +1,29 @@
 import Image from 'next/image'
 import styles from './notFound.module.scss'
 import NavigationLink from '@/components/ui/NavigationLink/NavigationLink'
+import { useTranslations } from 'next-intl'
 
 import notFound from '@/assets/404.png'
 import { FaHome } from 'react-icons/fa'
 
 export default function NotFoundPage() {
+  const t = useTranslations('404page')
+
   return (
     <div className={styles.container}>
       <div>
-        <Image src={notFound} alt='Page not found' />
+        <Image src={notFound} alt={t('page_not_found')} />
       </div>
       <div className={styles.message}>
-        <h1>Page not found</h1>
-        <h4>Sorry, the page you were looking for does not exist.</h4>
+        <h1>{t('page_not_found')}</h1>
+        <h4>{t('sorry_the_page')}</h4>
 
         <NavigationLink href='/' className={styles.link}>
-          <FaHome /> Back Home
+          <FaHome /> {t('back_home')}
         </NavigationLink>
 
         <span>
-          If the page is broken, <NavigationLink href='/contact-us'>Let us know</NavigationLink>.
+          {t('if_page_broken')} <NavigationLink href='/contact-us'>{t('let_us_know')}</NavigationLink> .
         </span>
       </div>
     </div>
