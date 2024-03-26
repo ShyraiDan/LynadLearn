c13e374da45c9ccc1429f82abe970f1fe9b883d5
ShirayDan <dan.shirayy@gmail.com>
2024-03-26 10:16:50 +0200

    rotating table in vocabulary page

--- src/app/[locale]/dashboard/vocabulary/page.tsx
+++ src/app/[locale]/dashboard/vocabulary/page.tsx
@@ -1,11 +1,9 @@
-import SideBar from '@/components/SideBar/SideBar'
 import WordsTable from '@/components/WordsTable/WordsTable'
 import styles from './VocabularyPage.module.scss'
 
 export default function VocabularyPage() {
   return (
     <div className={styles.container}>
-      <SideBar />
       <WordsTable />
     </div>
   )
