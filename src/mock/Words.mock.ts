2b0d0fbd9e647a55f9860bd4a4d28146761a1c75
ShyraiDan <dan.shirayy@gmail.com>
2024-03-30 11:17:57 +0200

    changing user settings

--- src/mock/Words.mock.ts
+++ src/mock/Words.mock.ts
@@ -1,101 +1,101 @@
 import { IWord } from '@/interfaces/Word.interface'
 
 export const DWords: IWord[] = [
-	{
-		id: 1,
-		word: 'Serendipity',
-		part_of_speech: 'Noun',
-		definition: 'The occurrence of events by chance in a happy or beneficial way.',
-		pronunciation: '/ˌsɛrənˈdɪpɪti/',
-		translation: '',
-		example:
-			'It was serendipity that led me to discover my favorite bookstore while wandering through the streets of Paris.'
-	},
-	{
-		id: 2,
-		word: 'Ephemeral',
-		part_of_speech: 'Adjectives',
-		definition: 'Lasting for a very short time.',
-		pronunciation: '/ɪˈfɛm(ə)r(ə)l/',
-		translation: '',
-		example: "The beauty of the ephemeral cherry blossoms reminds us of life's fleeting nature."
-	},
-	{
-		id: 3,
-		word: 'Ubiquitous',
-		part_of_speech: 'Adjective',
-		definition: 'Present, appearing, or found everywhere.',
-		pronunciation: '/juːˈbɪkwɪtəs/',
-		translation: '',
-		example:
-			'Smartphones have become ubiquitous in modern society, revolutionizing the way we communicate and access information.'
-	},
-	{
-		id: 4,
-		word: 'Resilience',
-		part_of_speech: 'Noun',
-		definition: 'The capacity to recover quickly from difficulties; toughness.',
-		pronunciation: '/rɪˈzɪljəns/',
-		translation: '',
-		example:
-			'Despite facing numerous setbacks, her resilience and determination helped her overcome every obstacle in her path to success.'
-	},
-	{
-		id: 5,
-		word: 'Melancholy',
-		part_of_speech: 'Noun/Adjective',
-		definition: 'A feeling of pensive sadness, typically with no obvious cause.',
-		pronunciation: '/ˈmɛlənkəli/',
-		translation: '',
-		example:
-			'The melancholy melody of the violin echoed through the empty halls, evoking memories of lost love and distant dreams.'
-	},
-	{
-		id: 6,
-		word: 'Auspicious',
-		part_of_speech: 'Adjective',
-		definition: 'Conducive to success; favorable.',
-		pronunciation: '/ɔːˈspɪʃəs/',
-		translation: '',
-		example: 'The rainbow after the storm was seen as an auspicious sign of better times ahead.'
-	},
-	{
-		id: 7,
-		word: 'Eloquent',
-		part_of_speech: 'Adjective',
-		definition: 'Fluent or persuasive in speaking or writing.',
-		pronunciation: '/ˈɛləkwənt/',
-		translation: '',
-		example: 'His eloquent speech moved the audience to tears, leaving a lasting impact on everyone who listened.'
-	},
-	{
-		id: 8,
-		word: 'Indomitable',
-		part_of_speech: 'Adjective',
-		definition: 'Impossible to subdue or defeat.',
-		pronunciation: '/ɪnˈdɒmɪtəb(ə)l/',
-		translation: '',
-		example:
-			'Despite facing overwhelming odds, her indomitable spirit never wavered, inspiring others to persevere in the face of adversity.'
-	},
-	{
-		id: 9,
-		word: 'Tranquil',
-		part_of_speech: 'Adjective',
-		definition: 'Free from disturbance; calm.',
-		pronunciation: '/ˈtraŋkwɪl/',
-		translation: '',
-		example:
-			'The tranquil waters of the lake reflected the serene beauty of the surrounding mountains, creating a peaceful atmosphere.'
-	},
-	{
-		id: 10,
-		word: 'Voracious',
-		part_of_speech: 'Adjective',
-		definition: 'Having a very eager approach to an activity; consuming or eager to consume great amounts of food.',
-		pronunciation: '/vəˈreɪʃəs/',
-		translation: '',
-		example:
-			'With a voracious appetite for knowledge, she devoured books on a wide range of subjects, always seeking to expand her understanding of the world.'
-	}
+  {
+    id: 1,
+    word: 'Serendipity',
+    part_of_speech: 'Noun',
+    definition: 'The occurrence of events by chance in a happy or beneficial way.',
+    pronunciation: '/ˌsɛrənˈdɪpɪti/',
+    translation: '',
+    example:
+      'It was serendipity that led me to discover my favorite bookstore while wandering through the streets of Paris.'
+  },
+  {
+    id: 2,
+    word: 'Ephemeral',
+    part_of_speech: 'Adjectives',
+    definition: 'Lasting for a very short time.',
+    pronunciation: '/ɪˈfɛm(ə)r(ə)l/',
+    translation: '',
+    example: "The beauty of the ephemeral cherry blossoms reminds us of life's fleeting nature."
+  },
+  {
+    id: 3,
+    word: 'Ubiquitous',
+    part_of_speech: 'Adjective',
+    definition: 'Present, appearing, or found everywhere.',
+    pronunciation: '/juːˈbɪkwɪtəs/',
+    translation: '',
+    example:
+      'Smartphones have become ubiquitous in modern society, revolutionizing the way we communicate and access information.'
+  },
+  {
+    id: 4,
+    word: 'Resilience',
+    part_of_speech: 'Noun',
+    definition: 'The capacity to recover quickly from difficulties; toughness.',
+    pronunciation: '/rɪˈzɪljəns/',
+    translation: '',
+    example:
+      'Despite facing numerous setbacks, her resilience and determination helped her overcome every obstacle in her path to success.'
+  },
+  {
+    id: 5,
+    word: 'Melancholy',
+    part_of_speech: 'Noun/Adjective',
+    definition: 'A feeling of pensive sadness, typically with no obvious cause.',
+    pronunciation: '/ˈmɛlənkəli/',
+    translation: '',
+    example:
+      'The melancholy melody of the violin echoed through the empty halls, evoking memories of lost love and distant dreams.'
+  },
+  {
+    id: 6,
+    word: 'Auspicious',
+    part_of_speech: 'Adjective',
+    definition: 'Conducive to success; favorable.',
+    pronunciation: '/ɔːˈspɪʃəs/',
+    translation: '',
+    example: 'The rainbow after the storm was seen as an auspicious sign of better times ahead.'
+  },
+  {
+    id: 7,
+    word: 'Eloquent',
+    part_of_speech: 'Adjective',
+    definition: 'Fluent or persuasive in speaking or writing.',
+    pronunciation: '/ˈɛləkwənt/',
+    translation: '',
+    example: 'His eloquent speech moved the audience to tears, leaving a lasting impact on everyone who listened.'
+  },
+  {
+    id: 8,
+    word: 'Indomitable',
+    part_of_speech: 'Adjective',
+    definition: 'Impossible to subdue or defeat.',
+    pronunciation: '/ɪnˈdɒmɪtəb(ə)l/',
+    translation: '',
+    example:
+      'Despite facing overwhelming odds, her indomitable spirit never wavered, inspiring others to persevere in the face of adversity.'
+  },
+  {
+    id: 9,
+    word: 'Tranquil',
+    part_of_speech: 'Adjective',
+    definition: 'Free from disturbance; calm.',
+    pronunciation: '/ˈtraŋkwɪl/',
+    translation: '',
+    example:
+      'The tranquil waters of the lake reflected the serene beauty of the surrounding mountains, creating a peaceful atmosphere.'
+  },
+  {
+    id: 10,
+    word: 'Voracious',
+    part_of_speech: 'Adjective',
+    definition: 'Having a very eager approach to an activity; consuming or eager to consume great amounts of food.',
+    pronunciation: '/vəˈreɪʃəs/',
+    translation: '',
+    example:
+      'With a voracious appetite for knowledge, she devoured books on a wide range of subjects, always seeking to expand her understanding of the world.'
+  }
 ]
