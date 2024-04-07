4707387c2d017a06d5f90a1069b57149996a75c3
ShyraiDan <dan.shirayy@gmail.com>
2024-04-07 15:14:42 +0300

    created quiz page

--- src/app/[locale]/dashboard/quiz/page.tsx
+++ src/app/[locale]/dashboard/quiz/page.tsx
@@ -1,7 +1,31 @@
+'use client'
+
+import styles from './QuizPage.module.scss'
+import { useState } from 'react'
+
 export default function QuizPage() {
+  const [type, setType] = useState('grammar')
+
   return (
-    <main>
-      <h1>Quiz page</h1>
-    </main>
+    <>
+      <div className={styles.container}>
+        <h2>Quiz page</h2>
+        <div className={styles.sections}>
+          <div className={styles.top}>
+            <h4>Filter</h4>
+            <div className={styles.tags}>
+              <span onClick={() => setType('grammar')} className={`${type === 'grammar' && styles.active}`}>
+                Grammar
+              </span>
+              <span onClick={() => setType('vocabulary')} className={`${type === 'vocabulary' && styles.active}`}>
+                Vocabulary
+              </span>
+            </div>
+          </div>
+          <div className={styles.items}></div>
+        </div>
+        <div className={styles.sections}></div>
+      </div>
+    </>
   )
 }
