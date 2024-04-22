10acc5a48c305d93c38982c7bf46943677b32cc5
ShyraiDan <dan.shirayy@gmail.com>
2024-04-22 08:36:14 +0300

    created progress bar

--- src/components/Achievements/Achievements.tsx
+++ src/components/Achievements/Achievements.tsx
@@ -4,11 +4,14 @@ import { FaUser } from 'react-icons/fa'
 
 export default function Achievements() {
   return (
-    <div className={styles.achievement}>
+    <div className={`${styles.achievement} ${styles.reached}`}>
       <div className={styles.icon}>
         <FaUser />
       </div>
       <h6>Achievement</h6>
+      <div className={styles.progress}>
+        <div style={{ width: '70%' }}></div>
+      </div>
       <p>Learn 10 words</p>
     </div>
   )
