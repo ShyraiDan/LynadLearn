6c04aa48615ef829ed2d7826999f1857d69ee1cd
ShyraiDan <dan.shirayy@gmail.com>
2024-05-12 12:42:19 +0300

    added link to vocabulary list

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,12 +1,15 @@
 import styles from './List.module.scss'
 import { IList } from '@/interfaces/List.interface'
 import Image from 'next/image'
+import NavigationLink from '../ui/NavigationLink/NavigationLink'
 
 export default function List({ id, title, image }: IList) {
   return (
     <div key={id} className={styles.container}>
-      <div className={styles['image-cont']}>{image && <Image src={image} alt={title} />}</div>
-      <span className={styles.title}>{title}</span>
+      <NavigationLink href={`/dashboard/vocabulary`}>
+        <div className={styles['image-cont']}>{image && <Image src={image} alt={title} />}</div>
+        <span className={styles.title}>{title}</span>
+      </NavigationLink>
     </div>
   )
 }
