a50ea6d52fb1d4beccab1a275806dc4a628e4a9b
ShyraiDan <dan.shirayy@gmail.com>
2024-12-31 17:51:23 +0200

    Fix: Refactored Container component

--- src/components/ui/Container/Container.tsx
+++ src/components/ui/Container/Container.tsx
@@ -1,13 +1,17 @@
 // TODO check throw the hole app scss files where I can use it
 
 import React, { forwardRef } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 type ContainerProps = React.HTMLAttributes<HTMLDivElement>
 
 const Container = forwardRef<HTMLDivElement, ContainerProps>(({ children, className, ...props }, ref) => {
   return (
     <div
-      className={`p-4 min-h-[calc(100vh-397px-73px)] sm:min-h-[calc(100vh-193px-81px)] md:min-h-[calc(100vh-153px-81px)] lg:min-h-[calc(100vh-97px-81px)] ${className}`}
+      className={twMerge(
+        'p-4 min-h-[calc(100vh-397px-73px)] sm:min-h-[calc(100vh-193px-81px)] md:min-h-[calc(100vh-153px-81px)] lg:min-h-[calc(100vh-97px-81px)]',
+        className
+      )}
       ref={ref}
       {...props}>
       {children}
