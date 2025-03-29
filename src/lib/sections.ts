6cdf5b12bf0deea47e95492208a8a0b3a871e38f
ShyraiDan <dan.shirayy@gmail.com>
2025-03-29 19:21:48 +0200

    Fix: Moved back sections API

--- src/lib/sections.ts
+++ src/lib/sections.ts
@@ -1,23 +1,15 @@
 'use server'
 
-import Sections from '@/interfaces/Section.interface'
+import Sections, { ISection } from '@/interfaces/Section.interface'
 import DefaultWords from '@/interfaces/defaultWords.interface'
-import { NextResponse } from 'next/server'
 
-export const getSectionById = async (id: string): Promise<NextResponse> => {
-  try {
-    await DefaultWords
-    const sections = await Sections.findById(id).populate(
-      'words',
-      'word results pronunciation translation listId common_adverb common_noun common_verb common_adjective common_phrase'
-    )
+export const getSectionById = async (id: string): Promise<ISection> => {
+  await DefaultWords
+  const sections = await Sections.findById(id).populate(
+    'words',
+    'word results pronunciation translation listId common_adverb common_noun common_verb common_adjective common_phrase'
+  )
+  const data = JSON.parse(JSON.stringify(sections))
 
-    if (!sections) {
-      return NextResponse.json({ message: 'Section not found' }, { status: 404 })
-    }
-
-    return NextResponse.json(sections)
-  } catch (error) {
-    return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 })
-  }
+  return data
 }
